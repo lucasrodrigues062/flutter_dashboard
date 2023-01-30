d033c7de224ae45a252e5f64a002e1fe4db67945
Lucas Rodrigues <lucasrodrigues062@gmail.com>
2023-01-30 11:37:07 -0300

    TOP BAR Criado

--- lib/site_layout.dart
+++ lib/site_layout.dart
@@ -1,19 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dashboard/helpers/responsive_widget.dart';
 import 'package:flutter_dashboard/widgets/large_screen.dart';
-import 'package:flutter_dashboard/widgets/small_screen%20copy.dart';
+import 'package:flutter_dashboard/widgets/medium_screen.dart';
 import 'package:flutter_dashboard/widgets/small_screen.dart';
+import 'package:flutter_dashboard/widgets/top_nav.dart';
 
 class SiteLayout extends StatelessWidget {
-  const SiteLayout({Key? key}) : super(key: key);
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey();
+
+  SiteLayout({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.white,
+      key: scaffoldKey,
+      appBar: topNavigationBar(
+        context,
+        scaffoldKey,
       ),
+      drawer: const Drawer(),
       body: const ResponsiveWidget(
         largeScreen: LargeScreen(),
         smallScreen: SmallScreen(),
---
+++ lib/widgets/top_nav.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dashboard/constants/style.dart';
+import 'package:flutter_dashboard/helpers/responsive_widget.dart';
+import 'package:flutter_dashboard/widgets/custom_text.dart';
+
+AppBar topNavigationBar(BuildContext context, GlobalKey<ScaffoldState> key) =>
+    AppBar(
+      elevation: 0,
+      leading: !ResponsiveWidget.isSmallScreen(context)
+          ? Row(
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(left: 14),
+                  child: Image.asset(
+                    'assets/icons/logo.png',
+                    width: 28,
+                  ),
+                )
+              ],
+            )
+          : IconButton(
+              onPressed: () {
+                key.currentState?.openDrawer();
+              },
+              icon: const Icon(Icons.menu)),
+      title: Row(
+        children: [
+          const Visibility(
+            child: CustomText(
+              text: 'Dash',
+              color: lightGrey,
+              size: 20,
+              weight: FontWeight.bold,
+            ),
+          ),
+          Expanded(child: Container()),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.settings,
+              color: dark.withOpacity(.7),
+            ),
+          ),
+          Stack(
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.notifications,
+                  color: dark.withOpacity(.7),
+                ),
+              ),
+              Positioned(
+                top: 7,
+                right: 7,
+                child: Container(
+                  width: 12,
+                  height: 12,
+                  padding: const EdgeInsets.all(4),
+                  decoration: BoxDecoration(
+                      color: active,
+                      borderRadius: BorderRadius.circular(30),
+                      border: Border.all(color: light, width: 2)),
+                ),
+              )
+            ],
+          ),
+          Container(
+            width: 1,
+            height: 22,
+            color: lightGrey,
+          ),
+          const SizedBox(
+            width: 24,
+          ),
+          const CustomText(
+            text: 'Lucas Rodrigues',
+            color: lightGrey,
+          ),
+          const SizedBox(
+            width: 16,
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(30),
+            ),
+            child: Container(
+              padding: const EdgeInsets.all(2),
+              margin: const EdgeInsets.all(2),
+              child: const CircleAvatar(
+                backgroundColor: light,
+                child: Icon(
+                  Icons.person_outline,
+                  color: dark,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+      iconTheme: const IconThemeData(color: dark),
+      backgroundColor: Colors.transparent,
+    );
