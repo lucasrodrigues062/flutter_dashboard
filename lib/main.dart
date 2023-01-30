f1c812816817bac2895df2ac9dfb464edc002376
Lucas Rodrigues <lucasrodrigues062@gmail.com>
2023-01-30 12:07:40 -0300

    Criado Menu Controller

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
+  Get.put(MenuController());
   runApp(const MyApp());
 }
 
@@ -16,18 +17,14 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Dash',
       theme: ThemeData(
-        scaffoldBackgroundColor: Colors.white,
-        textTheme: GoogleFonts.mulishTextTheme(
-          Theme.of(context).textTheme
-        ).apply(
-          bodyColor: Colors.black
-        ),
-        pageTransitionsTheme: const PageTransitionsTheme(builders: {
-          TargetPlatform.iOS: FadeUpwardsPageTransitionsBuilder(),
-          TargetPlatform.android: FadeUpwardsPageTransitionsBuilder(),
-        }),
-        primaryColor: Colors.blue
-      ),
+          scaffoldBackgroundColor: Colors.white,
+          textTheme: GoogleFonts.mulishTextTheme(Theme.of(context).textTheme)
+              .apply(bodyColor: Colors.black),
+          pageTransitionsTheme: const PageTransitionsTheme(builders: {
+            TargetPlatform.iOS: FadeUpwardsPageTransitionsBuilder(),
+            TargetPlatform.android: FadeUpwardsPageTransitionsBuilder(),
+          }),
+          primaryColor: Colors.blue),
       home: SiteLayout(),
     );
   }
---
+++ lib/routing/routes.dart
@@ -0,0 +1,11 @@
+const overViewPageRoute = 'Overview';
+const driversPageRoute = 'Drivers';
+const clientsPageRoute = 'Clients';
+const authenticationPageRoute = 'Authentication';
+
+List sideMenuitems = [
+  overViewPageRoute,
+  driversPageRoute,
+  clientsPageRoute,
+  authenticationPageRoute
+];
